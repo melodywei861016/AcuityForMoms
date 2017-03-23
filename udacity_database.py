cc56b32aaf4249040bdf1224cf6fb3e596737dad
celenac <earthfairy98@yahoo.com>
2017-03-23 07:39:34 -0700

    Assigned dictionary['price']
    Changed dictionary['price'] so that it leads to the homepage of the
    degree earned

--- udacity_database.py
+++ udacity_database.py
@@ -4,25 +4,24 @@ response = urllib.request.urlopen('https://udacity.com/public-api/v0/courses')
 json_response = json.loads(response.read())
 database = []
 for course in json_response['courses']:
-	dictionary = {}
+    dictionary = {}
     dictionary['title'] = course['title']
     dictionary['homepage'] = course['homepage']
     dictionary['short description'] = course['short_summary']
-    dictionary['level'] = course['level']
+    dictionary['level'] = course['level'] + course['starter'] #course['starter'] tells if the course is a starter course or not (true/false)
     dictionary['prerequisites'] = course['required_knowledge']
     dictionary['expected learning'] = course['expected_learning']
-    dictionary['image'] = course['level']
+    dictionary['image'] = course['image']
     dictionary['owner name'] = course['affiliates']['name'] #not sure if this works
     dictionary['time to complete'] = course['expected_duration'] + " " + course[expected_duration_unit]
-    dictionary['price'] =  
-
-
-
+    dictionary['price'] = degrees['homepage'] #leads to the homepage of the degree 
+    
     print (course['title'])
-    print (course['homepage'])
+    print (course['affiliates'])
 
 
-The important data that we need: title
+"""The important data that we need:
+title
 homepage
 short description
 level
@@ -33,4 +32,4 @@ owner name (what university)
 time to complete
 price
 if free price of the certificate
-time frame when you can follow the course (some courses are not always available)
+time frame when you can follow the course (some courses are not always available)"""
